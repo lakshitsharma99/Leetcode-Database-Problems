85da3a1c01d8e8f4aab6f37a69a18b6802348cde
Lakshit Sharma <lakshitsharma9915@gmail.com>
2022-02-07 01:38:29 +0530

    183. Customers Who Never Order

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,13 @@
+-- Write an SQL query to report all customers who never order anything.
+LeetCode: https://leetcode.com/problems/customers-who-never-order/submissions/
+
+SELECT 
+    name AS Customers
+FROM
+    (SELECT 
+        c.*, o.customerid AS orderID
+    FROM
+        customers c
+    LEFT JOIN orders o ON c.id = o.customerid) x
+WHERE
+    orderID IS NULL;
